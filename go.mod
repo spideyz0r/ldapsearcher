5b9f8c8ae137043d711dbf07f5a82febaf6ec6bc
spideyz0r <47341410+spideyz0r@users.noreply.github.com>
2023-03-04 02:17:45 -0500

    more features

--- go.mod
+++ go.mod
@@ -3,9 +3,14 @@ module github.com/spideyz0r/ldapsearcher
 go 1.18
 
 require (
+	github.com/go-ldap/ldap/v3 v3.4.4
+	github.com/pborman/getopt v1.1.0
+	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1
+)
+
+require (
 	github.com/Azure/go-ntlmssp v0.0.0-20220621081337-cb9428e4ac1e // indirect
 	github.com/go-asn1-ber/asn1-ber v1.5.4 // indirect
-	github.com/go-ldap/ldap/v3 v3.4.4 // indirect
-	github.com/pborman/getopt v1.1.0 // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 )
--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
+	"strings"
 
 	"github.com/go-ldap/ldap/v3"
 	"github.com/pborman/getopt"
@@ -15,7 +15,15 @@ func main () {
     ldap_port := getopt.StringLong("port", 'p', "389", "ldap port")
     ldap_user := getopt.StringLong("user", 'u', "", "username")
     base_dn := getopt.StringLong("base-dn", 'd', "", "BaseDN")
-    group_search := getopt.StringLong("list-members-group", 'l', "", "list members of a group")
+    group_memberof := getopt.StringLong("group-membersof", 'm', "", "list groups that the specified group is a member, wildcards allowed")
+    user_group_search := getopt.StringLong("list-groups-user", 'g', "", "list groups of a user")
+    group_member_list := getopt.StringLong("list-all-members-group", 'l', "", "list all members of a group -- usually requires full distinguished name of group")
+    extra_ou := getopt.StringLong("extra-ou", 'o', "", "extra organizational unit")
+    modified_after := getopt.StringLong("modified-after", 'c', "", "list users created or modified after date YYYYMMDDHHMMSS")
+    recursive_groups_user := getopt.StringLong("recursive-list-group", 'r', "", "list nested groups for a user")
+    users_locked := getopt.BoolLong("locked", 'k', "list locked users")
+
+
     getopt.Parse()
 
     if *help {
@@ -30,70 +38,168 @@ func main () {
     }
     defer conn.Close()
 
-    if len(*group_search) >0 {
-        memberOfGroup(conn, *group_search, *base_dn)
+    if len(*group_memberof) >0 {
+        memberOfGroup(conn, *group_memberof, *base_dn, true)
+        os.Exit(0)
     }
 
-    fmt.Printf("%s %s \n", *ldap_user, *ldap_server)
+    if len(*user_group_search) >0 {
+        listGroupsUser(conn, *user_group_search, *base_dn)
+        os.Exit(0)
+    }
+
+    if len(*group_member_list) >0 {
+        listAllMembersGroup(conn, *group_member_list, *base_dn, *extra_ou)
+        os.Exit(0)
+    }
+
+    if len(*modified_after) >0 {
+        usersModifiedAfterDate(conn, *modified_after, *base_dn)
+        os.Exit(0)
+    }
+
+    if len(*recursive_groups_user) >0 {
+        listNestedGroupsUser(conn, *recursive_groups_user, *base_dn, *extra_ou)
+        os.Exit(0)
+    }
+
+    if *users_locked {
+        lockedUsers(conn, *base_dn)
+        os.Exit(0)
+    }
     os.Exit(0)
 }
 
 func ldapConnect(server string, user string)(*ldap.Conn, error){
-
     conn, err := ldap.Dial("tcp", server)
-    fmt.Printf("Connecting to LDAP...\n")
     if err != nil {
         return nil, fmt.Errorf("Failed to connect to server: %s\n", err)
     }
     if err := conn.Bind(user, os.Getenv("ADP")); err != nil {
         return nil, fmt.Errorf("Failed to bind: %s\n", err)
     }
-    fmt.Printf("Connected!\n")
 
     return conn, nil
 }
 
-// search members from group
-func memberOfGroup (conn *ldap.Conn, group string, base_dn string) {
-    fmt.Printf("bah\n")
+func runQuery(conn *ldap.Conn, base_dn string, filter string, attributes []string)(*ldap.SearchResult, error){
     searchRequest := ldap.NewSearchRequest(
         base_dn,
         2, 0, 0, 0, false,
-        "(cn="+ group +")",
-        []string{"cn", "sAMAccountName", "memberOf"},
+        filter,
+        attributes,
         nil,
     )
+    sr, err := conn.Search(searchRequest);
 
-    sr, err := conn.Search(searchRequest)
     if err != nil {
-        log.Fatal(err)
+        return nil, fmt.Errorf("Failed to run search: %s\n", err)
+    }
+    return sr, nil
+}
+
+func printOut(sr *ldap.SearchResult, attributes []string, header bool)(){
+    if len(sr.Entries) <= 0{
+        fmt.Printf("No results found.\n")
+    }
+    for _, s := range sr.Entries {
+        for _, a := range attributes{
+            if header {
+                fmt.Printf("%s:\n", a)
+            }
+            for _, i := range s.GetAttributeValues(a) {
+                fmt.Printf("\t%s\n", i)
+            }
+        }
+        fmt.Printf("\n")
+    }
+}
+
+func memberOfGroup (conn *ldap.Conn, group string, base_dn string, printout bool)(*ldap.SearchResult, error) {
+    sr, err := runQuery(conn, base_dn, "(cn="+ group +")", []string{"cn", "sAMAccountName", "memberOf"})
+    if err != nil {
+        fmt.Printf("Query failed %s\n", err)
+        os.Exit(2)
     }
 
-    for _, s:= range sr.Entries {
-        fmt.Printf("----\n")
-        s.Print()
-        fmt.Printf("----\n")
-    }
     // sr.Entries[0].Print()
-    // fmt.Printf("%+v\n", sr)
+    if printout {
+        printOut(sr, []string{"cn", "memberOf"}, true)
+    }
+    return sr, nil
 }
 
-// search groups from user
-func groupSearch () {
-    fmt.Printf("Searching group\n")
+func listGroupsUser (conn *ldap.Conn, user string, base_dn string) {
+    sr, err := runQuery(conn, base_dn, "(mail="+ user +")", []string{"sAMAccountName", "memberOf"})
+    if err != nil {
+        fmt.Printf("Query failed %s\n", err)
+        os.Exit(2)
+    }
+    printOut(sr, []string{"sAMAccountName", "memberOf"}, true)
 }
 
+func listNestedGroupsUser (conn *ldap.Conn, user string, base_dn string, extra_ou string) {
+    sr, err := runQuery(conn, base_dn, "(mail="+ user +")", []string{"sAMAccountName", "memberOf"})
+    if err != nil {
+        fmt.Printf("Query failed %s\n", err)
+        os.Exit(2)
+    }
+
+    level := []string{">"}
+    for _, s := range sr.Entries {
+        for _, i := range s.GetAttributeValues("memberOf") {
+            group := strings.Split(strings.Split(i, "=")[1], ",")[0]
+            fmt.Printf(">>>>>> %s\n", group)
+            recursiveSearch(conn, base_dn, group, level)
+        }
+    }
+}
+
+func recursiveSearch(conn *ldap.Conn, base_dn string, g string, level []string)() {
+    sr, err := memberOfGroup(conn, g, base_dn, false)
+    if err != nil {
+        fmt.Printf("Query failed %s\n", err)
+        os.Exit(2)
+    }
+    for _, s := range sr.Entries {
+        for _, i := range s.GetAttributeValues("memberOf"){
+            group := strings.Split(strings.Split(i, "=")[1], ",")[0]
+            level = append(level, ">")
+            fmt.Printf("Inner group %v:  %s\n", level, group)
+            recursiveSearch(conn, base_dn, group, level)
+        }
+    }
+}
+
+func listAllMembersGroup (conn *ldap.Conn, group string, base_dn string, extra_ou string) {
+    sr, err := runQuery(conn, base_dn, "(memberOf=cn="+ group + "," + extra_ou + "," + base_dn +")", []string{"sAMAccountName"})
+    if err != nil {
+        fmt.Printf("Query failed %s\n", err)
+        os.Exit(2)
+    }
+    printOut(sr, []string{"sAMAccountName"}, false)
+}
+
+func usersModifiedAfterDate (conn *ldap.Conn, date string, base_dn string) {
+    sr, err := runQuery(conn, base_dn, "(&(whenChanged>="+ date +".0-0500)(objectClass=user)(objectCategory=person))", []string{"sAMAccountName"})
+    if err != nil {
+        fmt.Printf("Query failed %s\n", err)
+        os.Exit(2)
+    }
+    printOut(sr, []string{"sAMAccountName"}, false)
+}
+
+func lockedUsers (conn *ldap.Conn, base_dn string) {
+    sr, err := runQuery(conn, base_dn, "(lockoutTime>=1)", []string{"sAMAccountName"})
+    if err != nil {
+        fmt.Printf("Query failed %s\n", err)
+        os.Exit(2)
+    }
+    printOut(sr, []string{"sAMAccountName"}, false)
+}
 
 // TODO
 // deal with optional args
 // accept password via read or stdin
+// deal with json
 
-// Groups starting with certain string
-
-// Users created after x date
-
-// Users that belong to a group (Recursively)
-
-// Disabled user accounts
-
-// Locked user accounts
