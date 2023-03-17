47ff8e70ad5e3da9d32f1d2be377535452462be0
spideyz0r <47341410+spideyz0r@users.noreply.github.com>
2023-03-17 18:05:18 -0400

    go fmt, plus some improvements

--- main.go
+++ main.go
@@ -11,6 +11,11 @@ import (
 )
 
 func main() {
+	const (
+		emailFilter    = "(mail=%s)"
+		memberOfFilter = "(memberOf=cn=%s,%s,%s)"
+	)
+
 	help := getopt.BoolLong("help", 'h', "display this help")
 	ldap_server := getopt.StringLong("server", 's', "localhost", "ldap server")
 	ldap_port := getopt.StringLong("port", 'p', "389", "ldap port")
@@ -23,6 +28,9 @@ func main() {
 	modified_after := getopt.StringLong("modified-after", 'c', "", "list users created or modified after date YYYYMMDDHHMMSS")
 	recursive_groups_user := getopt.StringLong("recursive-list-group", 'r', "", "list nested groups for a user")
 	users_locked := getopt.BoolLong("locked", 'k', "list locked users")
+	custom_search := getopt.BoolLong("custom-search", 't', "", "custom search")
+	custom_filter := getopt.StringLong("custom-filter", 'f', "", "filter for custom search")
+	custom_attributes := getopt.StringLong("custom-attributes", 'a', "", "list of attributes delimited by ',' for custom search")
 
 	getopt.Parse()
 
@@ -37,87 +45,128 @@ func main() {
 	}
 	defer conn.Close()
 
-	if len(*group_memberof) > 0 {
-		_, err := memberOfGroup(conn, *group_memberof, *base_dn, true)
+	if *custom_search {
+		if len(*custom_filter) == 0 || len(*custom_attributes) == 0 {
+			log.Fatal("Custom search requires valid filter and attributes")
+		}
+		s := Search{
+			base_dn:    *base_dn,
+			filter:     *custom_filter,
+			attributes: strings.Split(*custom_attributes, ","),
+		}
+		sr, err := s.newSearch(conn)
 		if err != nil {
 			log.Fatal(err)
 		}
+		printOut(sr, s.attributes, true)
 		os.Exit(0)
 	}
 
-	if len(*user_group_search) > 0 {
-		err = listGroupsUser(conn, *user_group_search, *base_dn)
+	// switch {
+	// case  len(*user_group_search) > 0:
+	// ...
+	// case len(*group_member_list) > 0:
+	// ...
+	// case
+
+	// len(*modified_after) > 0
+
+	if len(*recursive_groups_user) > 0 {
+		s := Search{
+			base_dn:    *base_dn,
+			filter:     fmt.Sprintf("(mail=%s)", *recursive_groups_user),
+			attributes: []string{"sAMAccountName", "memberOf"},
+		}
+		sr, err := s.newSearch(conn)
 		if err != nil {
 			log.Fatal(err)
 		}
+		printOut(sr, s.attributes, true)
+		listNestedGroupsUser(conn, s, sr)
+
+		// if len(*recursive_groups_user) > 0 {
+		// 	err = listNestedGroupsUser(conn, *recursive_groups_user, *base_dn, *extra_ou)
+		// 	if err != nil {
+		// 		log.Fatal(err)
+		// 	}
+		// 	os.Exit(0)
+	}
+
+	if len(*user_group_search) > 0 {
+		s := Search{
+			base_dn:    *base_dn,
+			filter:     fmt.Sprintf("(mail=%s)", *user_group_search),
+			attributes: []string{"sAMAccountName", "memberOf"},
+		}
+		sr, err := s.newSearch(conn)
+		if err != nil {
+			log.Fatal(err)
+		}
+		printOut(sr, s.attributes, true)
 		os.Exit(0)
 	}
 
 	if len(*group_member_list) > 0 {
-		err = listAllMembersGroup(conn, *group_member_list, *base_dn, *extra_ou)
+		s := Search{
+			base_dn:    *base_dn,
+			filter:     fmt.Sprintf("(memberOf=cn=%s, %s, %s)", *group_member_list, *extra_ou, *base_dn),
+			attributes: []string{"sAMAccountName"},
+		}
+		sr, err := s.newSearch(conn)
 		if err != nil {
 			log.Fatal(err)
 		}
+		printOut(sr, s.attributes, true)
 		os.Exit(0)
 	}
 
 	if len(*modified_after) > 0 {
-		err = usersModifiedAfterDate(conn, *modified_after, *base_dn)
+		s := Search{
+			base_dn:    *base_dn,
+			filter:     fmt.Sprintf("(&(whenChanged>=%s.0-0500)(objectClass=user)(objectCategory=person))", *modified_after),
+			attributes: []string{"sAMAccountName"},
+		}
+		sr, err := s.newSearch(conn)
 		if err != nil {
 			log.Fatal(err)
 		}
-		os.Exit(0)
-	}
-
-	if len(*recursive_groups_user) > 0 {
-		err = listNestedGroupsUser(conn, *recursive_groups_user, *base_dn, *extra_ou)
-		if err != nil {
-			log.Fatal(err)
-		}
+		printOut(sr, s.attributes, true)
 		os.Exit(0)
 	}
 
 	if *users_locked {
-		err = lockedUsers(conn, *base_dn)
+		s := Search{
+			base_dn:    *base_dn,
+			filter:     "(lockoutTime>=1)",
+			attributes: []string{"sAMAccountName"},
+		}
+		sr, err := s.newSearch(conn)
 		if err != nil {
 			log.Fatal(err)
 		}
+		printOut(sr, s.attributes, true)
 		os.Exit(0)
 	}
-}
 
-func ldapConnect(server string, user string) (*ldap.Conn, error) {
-	conn, err := ldap.Dial("tcp", server)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to connect to server: %s\n", err)
+	if len(*group_memberof) > 0 {
+		s := Search{
+			base_dn:    *base_dn,
+			filter:     fmt.Sprintf("(cn=%s)", *group_memberof),
+			attributes: []string{"cn", "sAMAccountName", "memberOf"},
+		}
+		sr, err := s.newSearch(conn)
+		if err != nil {
+			log.Fatal(err)
+		}
+		printOut(sr, s.attributes, true)
+		os.Exit(0)
 	}
-	if err := conn.Bind(user, os.Getenv("ADP")); err != nil {
-		return nil, fmt.Errorf("Failed to bind: %s\n", err)
-	}
-
-	return conn, nil
-}
-
-func runQuery(conn *ldap.Conn, base_dn string, filter string, attributes []string) (*ldap.SearchResult, error) {
-	searchRequest := ldap.NewSearchRequest(
-		base_dn,
-		2, 0, 0, 0, false,
-		filter,
-		attributes,
-		nil,
-	)
-	sr, err := conn.Search(searchRequest)
-
-	if err != nil {
-		return nil, fmt.Errorf("Failed to run search: %s\n", err)
-	}
-	return sr, nil
 }
 
 func printOut(sr *ldap.SearchResult, attributes []string, header bool) {
 	if len(sr.Entries) <= 0 {
 		fmt.Printf("No results found.\n")
-    }
+	}
 	for _, s := range sr.Entries {
 		for _, a := range attributes {
 			if header {
@@ -131,97 +180,86 @@ func printOut(sr *ldap.SearchResult, attributes []string, header bool) {
 	}
 }
 
-func memberOfGroup(conn *ldap.Conn, group string, base_dn string, printout bool) (*ldap.SearchResult, error) {
-	sr, err := runQuery(conn, base_dn, "(cn="+group+")", []string{"cn", "sAMAccountName", "memberOf"})
-	if err != nil {
-		return nil, fmt.Errorf("query failed %s", err)
-	}
-
-	if printout {
-		printOut(sr, []string{"cn", "memberOf"}, true)
-	}
-	return sr, nil
-}
-
 func listGroupsUser(conn *ldap.Conn, user string, base_dn string) error {
-    attrs:= []string{"sAMAccountName", "memberOf"}
+	attrs := []string{"sAMAccountName", "memberOf"}
 
 	sr, err := runQuery(conn, base_dn, "(mail="+user+")", attrs)
 	if err != nil {
 		return fmt.Errorf("query failed %s", err)
 	}
 	printOut(sr, attrs, true)
-    return nil
-}
-
-func listNestedGroupsUser(conn *ldap.Conn, user string, base_dn string, extra_ou string) error {
-	sr, err := runQuery(conn, base_dn, "(mail="+user+")", []string{"sAMAccountName", "memberOf"})
-	if err != nil {
-		return fmt.Errorf("query failed %s", err)
-	}
-
-	level := []string{">"}
-	for _, s := range sr.Entries {
-		for _, i := range s.GetAttributeValues("memberOf") {
-			group := strings.Split(strings.Split(i, "=")[1], ",")[0]
-			fmt.Printf(">>>>>> %s\n", group)
-			err = recursiveSearch(conn, base_dn, group, level)
-            if err != nil {
-                return fmt.Errorf("query failed %s", err)
-            }
-		}
-	}
-    return nil
-}
-
-func recursiveSearch(conn *ldap.Conn, base_dn string, g string, level []string) error {
-	sr, err := memberOfGroup(conn, g, base_dn, false)
-	if err != nil {
-		return fmt.Errorf("query failed %s", err)
-	}
-	for _, s := range sr.Entries {
-		for _, i := range s.GetAttributeValues("memberOf") {
-			group := strings.Split(strings.Split(i, "=")[1], ",")[0]
-			level = append(level, ">")
-			fmt.Printf("Inner group %v:  %s\n", level, group)
-			recursiveSearch(conn, base_dn, group, level)
-		}
-	}
-    return nil
-}
-
-func listAllMembersGroup(conn *ldap.Conn, group string, base_dn string, extra_ou string) error {
-	filter := fmt.Sprintf("(memberOf=cn=%s, %s, %s)", group, extra_ou, base_dn)
-	attrs := []string{"sAMAccountName"}
-
-	sr, err := runQuery(conn, base_dn, filter, attrs)
-	if err != nil {
-		return fmt.Errorf("query failed %s", err)
-	}
-
-	printOut(sr, attrs, false)
 	return nil
 }
 
-func usersModifiedAfterDate(conn *ldap.Conn, date string, base_dn string) error {
-    attrs := []string{"sAMAccountName"}
-	sr, err := runQuery(conn, base_dn, "(&(whenChanged>="+date+".0-0500)(objectClass=user)(objectCategory=person))", attrs)
-	if err != nil {
-		return fmt.Errorf("query failed %s", err)
+type M map[string]interface{}
+
+func listNestedGroupsUser(conn *ldap.Conn, s Search, sr *ldap.SearchResult) error {
+	var myM []M
+	for _, e := range sr.Entries {
+		for _, i := range e.GetAttributeValues("memberOf") {
+			group := strings.Split(strings.Split(i, "=")[1], ",")[0]
+			fmt.Printf("%s\n", group)
+			myM = append(myM, recursiveSearch(conn, s.base_dn, group))
+			// if err != nil {
+			//     return fmt.Errorf("query failed %s", err)
+			// }
+		}
 	}
-	printOut(sr, attrs, false)
-    return nil
+	fmt.Printf("%v", myM)
+	return nil
 }
 
-func lockedUsers(conn *ldap.Conn, base_dn string) error {
-    attrs := []string{"sAMAccountName"}
-	sr, err := runQuery(conn, base_dn, "(lockoutTime>=1)", attrs)
-	if err != nil {
-		return fmt.Errorf("query failed %s", err)
+func recursiveSearch(conn *ldap.Conn, b string, g string) M {
+	var myM M
+	s := Search{
+		base_dn:    b,
+		filter:     fmt.Sprintf("(cn=%s)", g),
+		attributes: []string{"memberOf"},
 	}
-	printOut(sr, attrs, false)
-    return nil
+	sr, err := s.newSearch(conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, e := range sr.Entries {
+		for _, i := range e.GetAttributeValues("memberOf") {
+			group := strings.Split(strings.Split(i, "=")[1], ",")[0]
+			// fmt.Printf(">>>>>>>>>Inner group %s\n",group)
+			myM[group] = recursiveSearch(conn, group, s.base_dn)
+			// if err != nil {
+			//     return nil, fmt.Errorf("query failed %s", err)
+			// }
+		}
+	}
+	// fmt.Printf("%v", all_groups)
+	return myM
 }
+
+// func recursiveSearch(conn *ldap.Conn, b string, g string, m map[string][]string) ([]string error) {
+// 	s := Search {
+// 		base_dn: b,
+// 		filter: fmt.Sprintf("(cn=%s)", g),
+// 		attributes: []string{"memberOf"},
+// 	}
+// 	sr, err := s.newSearch(conn)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	for _, s := range sr.Entries {
+// 		for _, i := range s.GetAttributeValues("memberOf") {
+// 			group := strings.Split(strings.Split(i, "=")[1], ",")[0]
+// 			all_groups = append(all_groups, group)
+// 			// fmt.Printf(">>>>>>>>>Inner group %s\n", group)
+// 			m[group], err = recursiveSearch(conn, s.base_dn, group, all_groups)
+
+// 			err := recursiveSearch(conn, b, group, all_groups)
+// 			if err != nil {
+//                 return nil, fmt.Errorf("query failed %s", err)
+//             }
+// 		}
+// 	}
+// 	fmt.Printf("%v", all_groups)
+//     return nil,
+// }
 
 // TODO
 // deal with optional args
---
+++ search.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/go-ldap/ldap/v3"
+)
+
+type Search struct {
+	base_dn    string
+	filter     string
+	attributes []string
+	result     *ldap.SearchResult
+}
+
+func (s Search) newSearch(conn *ldap.Conn) (*ldap.SearchResult, error) {
+	searchRequest := ldap.NewSearchRequest(
+		s.base_dn,
+		2, 0, 0, 0, false,
+		s.filter,
+		s.attributes,
+		nil,
+	)
+	sr, err := conn.Search(searchRequest)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to run search: %s\n", err)
+	}
+	return sr, nil
+}
+
+func ldapConnect(server string, user string) (*ldap.Conn, error) {
+	conn, err := ldap.Dial("tcp", server)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to connect to server: %s\n", err)
+	}
+	if err := conn.Bind(user, os.Getenv("ADP")); err != nil {
+		return nil, fmt.Errorf("Failed to bind: %s\n", err)
+	}
+
+	return conn, nil
+}
+
+func runQuery(conn *ldap.Conn, base_dn string, filter string, attributes []string) (*ldap.SearchResult, error) {
+	searchRequest := ldap.NewSearchRequest(
+		base_dn,
+		2, 0, 0, 0, false,
+		filter,
+		attributes,
+		nil,
+	)
+	sr, err := conn.Search(searchRequest)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to run search: %s\n", err)
+	}
+	return sr, nil
+}
