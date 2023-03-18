5417cc41f815b12701915a4efd39d06f6d7fe5b1
spideyz0r <47341410+spideyz0r@users.noreply.github.com>
2023-03-17 22:49:29 -0400

    Update output and overall structure

--- search.go
+++ search.go
@@ -1,19 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/go-ldap/ldap/v3"
 )
 
 type Search struct {
-	base_dn    string
-	filter     string
-	attributes []string
-	result     *ldap.SearchResult
+	base_dn     string
+	filter      string
+	attributes  []string
+	result_json string
 }
 
-func (s Search) newSearch(conn *ldap.Conn) (*ldap.SearchResult, error) {
+func (s Search) newSearch(conn *ldap.Conn) (string, error) {
 	searchRequest := ldap.NewSearchRequest(
 		s.base_dn,
 		2, 0, 0, 0, false,
@@ -23,9 +24,38 @@ func (s Search) newSearch(conn *ldap.Conn) (*ldap.SearchResult, error) {
 	)
 	sr, err := conn.Search(searchRequest)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to run search: %s\n", err)
+		return "", fmt.Errorf("Failed to run search: %s\n", err)
 	}
-	return sr, nil
+	result_json, err := s.resultToJson(sr)
+	if err != nil {
+		return "", fmt.Errorf("Failed to convert search result to JSON: %s\n", err)
+	}
+	return result_json, nil
+}
+
+func (s Search) resultToJson(sr *ldap.SearchResult) (string, error) {
+	result := make(map[string][]string)
+	if len(sr.Entries) <= 0 {
+		return "{}", nil
+	}
+	for _, e := range sr.Entries {
+		for _, a := range s.attributes {
+			var results []string
+			for _, r := range e.GetAttributeValues(a) {
+				results = append(results, r)
+			}
+			if len(result[a]) > 0 {
+				result[a] = append(result[a], results...)
+			} else {
+				result[a] = results
+			}
+		}
+	}
+	j, err := json.Marshal(result)
+	if err != nil {
+		return "{}", err
+	}
+	return string(j), nil
 }
 
 func ldapConnect(server string, user string, pass string) (*ldap.Conn, error) {
